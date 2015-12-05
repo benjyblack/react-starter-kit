f42cc7ec4bf0817fd0e617bb84244fc9e599348e
Ben Black <ben.black@sweetiq.com>
2015-12-05 13:43:18 -0500

    Set up watchify and first React class

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,39 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
+var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 
-gulp.task('build', function () {
-  browserify({
-    entries: 'src/main.jsx',
+var build = function (shouldWatch) {
+  var bundler = browserify({
+    entries: 'public/src/main.jsx',
     extensions: ['.jsx'],
-    debug: true
-  })
-  .transform(babelify, { presets: ['es2015', 'react'] })
-  .bundle()
-  .pipe(source('bundle.js'))
-  .pipe(gulp.dest('dist'));
+    debug: true,
+    cache: {},
+    packageCache: {}
+  });
+
+  var bundle = function () {
+    return bundler.transform(babelify, { presets: ['es2015', 'react'] })
+      .bundle()
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('public/dist'));
+  };
+
+  if (shouldWatch) {
+    bundler = watchify(bundler);
+    bundler.on('update', bundle);
+  }
+
+  return bundle();
+};
+
+gulp.task('build', function () {
+  return build(false);
+});
+
+gulp.task('watch', function () {
+  return build(true);
 });
 
 gulp.task('default', ['build']);
--- public/src/main.jsx
+++ public/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentBox from './components/comment-box'
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <CommentBox />,
   document.getElementById('example')
 );
