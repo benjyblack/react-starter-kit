fcc30c3fa60bbc596a3a9ce3ebe888c9173bb003
Ben Black <ben.black@sweetiq.com>
2015-12-05 17:08:26 -0500

    Poll for data from API

--- public/src/components/CommentBox/CommentBox.js
+++ public/src/components/CommentBox/CommentBox.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import CommentList from '../CommentList/CommentList';
 import CommentForm from '../CommentForm/CommentForm';
 
 class CommentBox extends Component {
+  constructor() {
+    super();
+    this.state = { data: [] };
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer.bind(this), this.props.pollInterval);
+  }
+
+  loadCommentsFromServer() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
+
   render() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
